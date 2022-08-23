821e4879da40884f8cf4a4cc020de99190368e01
JumpyDumpty <111923130+JumpyDumpty@users.noreply.github.com>
2022-08-23 17:27:40 -0400

    Add files via upload

---
+++ prime_factors.py
@@ -0,0 +1,17 @@
+import math
+def prime_factors(num):
+    factor_list = []
+    while num % 2 == 0:
+        factor_list.append(2)
+        num = num/2
+    for a in range(3, int(math.sqrt(num))+1, 2):
+        while (num % a == 0):
+            factor_list.append(int(a))
+            num = num / a
+    if num > 2:
+        factor_list.append(int(num))
+    return factor_list
+
+print(prime_factors(20))
+print(prime_factors(100))
+print(prime_factors(114514))
